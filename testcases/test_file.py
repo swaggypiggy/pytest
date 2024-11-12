bc67d7af914654c10dfad9b4d628b32d23d483ec
swaggypiggy <1479538151@qq.com>
2024-11-12 22:25:06 +0800

    branch_test

--- testcases/test_file.py
+++ testcases/test_file.py
@@ -4,7 +4,7 @@ import requests
 
 class TestClass01:
 
-    @pytest.mark.parametrize("hello", [1, 2, 3])
+    @pytest.mark.parametrize("hello", [1, 2, 3, 5])
     def test_method01(self, hello):
         print(str(hello) + str(requests.get(url="https://www.baidu.com")))
         result = hello
