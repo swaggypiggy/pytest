b47bf22a7561a0216e852c176a023c6fc425ba74
swaggypiggy <1479538151@qq.com>
2024-11-12 22:50:55 +0800

    branch_test

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     pytest.main()
     time.sleep(3)
     os.system("allure generate ./temps -o ./reports --clean")
-    print("allure报告已生成！!")
+    print("allure报告已生成!!master!!")
